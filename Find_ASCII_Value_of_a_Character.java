83378326511c3f2115b90657e45a7981c5fe700e
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2023-06-30 21:05:55 +0530

    import java.util.Scanner; public class Arithmetic {   public static void main(String args[])   {       Scanner sc=new Scanner(System.in);       char str;       str=sc.next().charAt(0);       int a=str;       System.out.print(a);   } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Arithmetic
+{
+  public static void main(String args[])
+  {
+      Scanner sc=new Scanner(System.in);
+      char str;
+      str=sc.next().charAt(0);
+      int a=str;
+      System.out.print(a);
+  }
+}
